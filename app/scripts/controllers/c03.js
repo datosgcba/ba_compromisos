1d5b18b00459a1fd7771801b2ce0878bcc246e53
tinchoforever <tinchoforever@gmail.com>
2016-06-23 18:08:30 -0300

    3 graficos en adultos, ultimo andando

--- app/scripts/controllers/c03.js
+++ app/scripts/controllers/c03.js
@@ -63,5 +63,60 @@ angular.module('compromisosSiteApp')
       return data;
     };
 
+    //detalle 3
+
+    $scope.prepareData3 = function(data){
+      return data;
+    };
+
+    $scope.completeConfig3 = function(config){
+      return angular.merge(config,{
+        data:{
+          type: 'bar',
+          keys: {
+              value: ['poblacion_mayor_o_igual_65'],
+              x:'provincia'
+          },
+          colors: {'poblacion_mayor_o_igual_65':$scope.currentCompromise.color}
+        },
+        size: {
+            height: 300,
+        },
+        padding: {
+            top: 0,
+            right: 20,
+            bottom: 10,
+            left: 20,
+        },
+        axis: {
+          rotated:true,
+          x: {
+              type: 'category',
+              show:false
+          },
+          y: {
+              show:false
+          }
+        },
+        legend: {
+            show: false
+        }
+      });
+    };
+
+    $scope.chartReady3 = function(chart){
+
+    };
+
+    var id;
+    $(window).resize(function() {
+        clearTimeout(id);
+        id = setTimeout(function(){
+          // if(chart1){
+          //   createCustomChart1();
+          // }          
+        }, 500);
+    });
+
   	
   });
