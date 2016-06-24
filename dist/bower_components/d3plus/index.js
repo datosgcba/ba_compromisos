2605c84ffd806e2ad842e657c883695535c9f5e5
Pablo H. Paladino <paladinop@gmail.com>
2016-06-23 22:28:17 -0400

    build - bower components

--- dist/bower_components/d3plus/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require('./src/libs.coffee')
-
-module.exports = require('./src/init.coffee')
--- dist/bower_components/ng-youtube-embed/demo/app.js
+++
@@ -1,5 +0,0 @@
-var myApp = angular.module('myApp', ['ngYoutubeEmbed']);
-
-myApp.controller('myCtrl', ['$scope', function($scope) {
-    $scope.link = 'https://www.youtube.com/watch?v=OPmOXJtxxoo';
-}]);
