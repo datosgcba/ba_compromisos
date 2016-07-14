52e0f389c41387f3ab9870fee138c0e153dd5cb6
tinchoforever <tinchoforever@gmail.com>
2016-07-14 16:49:45 -0300

    grafico 12

--- app/scripts/controllers/c07.js
+++ app/scripts/controllers/c07.js
@@ -56,6 +56,7 @@ angular.module('compromisosSiteApp')
         d.linea_d_sec = toSeconds(d.linea_d);
         d.linea_e_sec = toSeconds(d.linea_e);
         d.linea_h_sec = toSeconds(d.linea_h);
+         d.total_sec = toSeconds(d.total);
       });
       return data;
     };
@@ -64,7 +65,7 @@ angular.module('compromisosSiteApp')
       return angular.merge(config,{
         data:{
           keys: {
-              value: ['linea_a_sec','linea_b_sec','linea_c_sec','linea_d_sec','linea_e_sec','linea_h_sec'],
+              value: ['linea_a_sec','linea_b_sec','linea_c_sec','linea_d_sec','linea_e_sec','linea_h_sec','total_sec'],
               x: 'mes_date'
           },
           names:{
@@ -73,7 +74,8 @@ angular.module('compromisosSiteApp')
             'linea_c_sec':'Línea C',
             'linea_d_sec':'Línea D',
             'linea_e_sec':'Línea E',
-            'linea_h_sec':'Línea H'
+            'linea_h_sec':'Línea H',
+            'total_sec':'Total'
           },
           type: 'spline'
 /*          colors: {'muertes': $scope.currentCompromise.color}*/
